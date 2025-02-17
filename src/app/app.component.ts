d42db51daf8d5b860afe43f63e22049204356f16
ronak.patel <ronak.patel@nationalautocare.com>
2025-02-17 21:06:58 +0530

    created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-18-with-ngx-toaster';
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Success');
+  }
+
+  showError() {
+    this.toastr.error('Something went wrong.', 'Error');
+  }
 }
